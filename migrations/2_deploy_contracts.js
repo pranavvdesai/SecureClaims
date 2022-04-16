d155f5037e734b93c42a1565324f67bad53a28cd
pranavvdesai <pranavdesai.psd@gmail.com>
2022-04-16 17:57:09 +0530

    feat: configs

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,14 +1,18 @@
 const Transactions = artifacts.require("Transactions");
+const Stake = artifacts.require("Stake");
 
 module.exports = async function (deployer, network, accounts) {
   await deployer.deploy(Transactions);
+  await deployer.deploy(Stake);
 
   const transactions = await Transactions.deployed();
 
+  const stake = await Stake.deployed();
+
   console.log("Transactions contract address: " + transactions.address);
+  console.log("Stake contract address: " + stake.address);
   
   // get balance of accounts[0]
   const balance = await transactions.getBalance(accounts[1]);
   console.log("Balance of accounts[1]: " + balance);
 };
- 
